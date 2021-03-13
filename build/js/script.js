c320a538558f577c0a79c7fb8499525196c1c87d
wurelcy <>
2021-03-13 16:06:21 +0300

    Реализует модалку

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const accordeonBlocks = document.querySelectorAll('.accordeon');
-const toggleButtons = document.querySelectorAll('.accordeon button');
+const callbackButton = document.querySelector('.header__call-button');
+const callbackCloseButton = document.querySelector('.callback-popup__button-close');
+const overlay = document.querySelector('.overlay');
+const callbackPopup = document.querySelector('.callback-popup');
+
+/*Footer Accordeon*/
 
 if (accordeonBlocks) {
   accordeonBlocks.forEach((block) => {
@@ -12,5 +17,43 @@ if (accordeonBlocks) {
   toggleButton.addEventListener('click', function() {
       block.classList.toggle('accordeon--closed');
     });
+  });
+}
+
+/*CallBack Form*/
+
+if (callbackButton) {
+  let closePopup = () => {
+    overlay.classList.add('overlay--hidden');
+    callbackPopup.classList.remove('pop-up--show');
+  };
+
+  let showPopup = () => {
+    overlay.classList.remove('overlay--hidden');
+    callbackPopup.classList.add('pop-up--show');
+  };
+
+  callbackButton.addEventListener('click', function() {
+    showPopup();
+  });
+
+  callbackButton.addEventListener('keydown', function(evt) {
+    if (evt.key === 'Enter') {
+      showPopup();
+    }
+  });
+
+  callbackCloseButton.addEventListener('click', function() {
+    closePopup();
+  });
+
+  overlay.addEventListener('click', function() {
+    closePopup();
+  });
+
+  window.addEventListener('keydown', function(evt) {
+    if (evt.key === 'Escape') {
+      closePopup();
+    }
   });
 }
